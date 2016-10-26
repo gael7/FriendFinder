a61b3f62ce2077f0da69dbca28228a1ac71c6f2b
Gael Arrambide <gael_arrambide@hotmail.com>
2016-10-26 18:32:17 -0500

    html pages done

---
+++ app/routing/api-routes.js
@@ -0,0 +1,29 @@
+app.get('/api/:friends?', function (req, res) {
+  var chosen = req.params.characters;
+
+	if (chosen) {
+		console.log(chosen);
+
+		for (var i = 0; i < friends.length; i++) {
+			if (chosen === friends[i].routeName) {
+				res.json(friends[i]);
+				return;
+			}
+		}
+
+		res.json(false);
+	} else {
+		res.json(friends);
+	}
+});
+
+app.post('/api/new', function (req, res) {
+	var newfriend = req.body;
+	console.log(newfriend);
+
+	// We then add the json the user sent to the character array
+	friends.push(newfriend);
+
+	// We then display the JSON to the users
+	res.json(newfriend);
+});
